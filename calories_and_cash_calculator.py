1da5cb93e0a42376544db3c1f1ae9f031277116d
chomyan <tsoi.rv@gmail.com>
2022-10-07 11:55:57 +0600

    initial commit

---
+++ calories_and_cash_calculator.py
@@ -0,0 +1,29 @@
+class Record:
+    def __init__(self, amount, comment, date):
+        self.amount = amount
+        self.comment = comment
+        self.date = date
+
+class Calculator:
+    def __init__(self) -> None:
+        pass
+
+    def add_record():
+        pass
+
+    def get_today_stats():
+        pass
+
+    def get_week_stats():
+        pass
+
+
+class Calories(Calculator):
+    pass
+
+
+
+class Cash(Calculator):
+    pass
+
+
